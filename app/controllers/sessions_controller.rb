30326dfd155bb137e95d68ea89884901ae432c2e
Brandon Felty <brcfelty@gmail.com>
2022-06-07 16:24:00 -0600

    fixes issue with login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,9 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by_email(params[:email])
+    user = User.authenticate_with_credentials(params[:email], params[:password])
     # If the user exists AND the password entered is correct.
-    if user && user.authenticate(params[:password])
+    if user
       # Save the user id inside the browser cookie. This is how we keep the user 
       # logged in when they navigate around our website.
       session[:user_id] = user.id
@@ -18,6 +18,6 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    redirect_to new_user_url
+    redirect_to new_session_url
   end
 end
